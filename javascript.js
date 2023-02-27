cc5d3df5cb81ee5abca17f9bfcf6f66561882e43
ZellyJelly <zellexepc@gmail.com>
2023-02-26 20:45:11 -0500

    New Project

---
+++ javascript.js
@@ -0,0 +1,10 @@
+function getComputerChoice()
+{
+
+}
+
+function generateRandomInteger(min, max) {
+    return Math.floor(min + Math.random()*(max - min + 1))
+  }
+
+  generateRandomInteger(1, 3);
